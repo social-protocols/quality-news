1767eb093c78331cfc852d79e2405a0718a08843
Joe Edmonds <joe-github-com@elem.com>
2022-11-14 11:29:16 -0800

    require entr and sqlite, set environment variables
    - with the goal of making local development easier on my laptop
    - environment variables copied from .envrc

--- shell.nix
+++ shell.nix
@@ -2,11 +2,17 @@
 
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
+      entr
       git
       gcc
       go_1_18
+      sqlite
     ];
 
+  shellHook = ''
+    export SQLITE_DATA_DIR=data
+    export CACHE_SIZE=100
+  '';
 
     installPhase= ''
     '';
