f0c77a39af146d1d35bdc7ca463ed6a40f513bbe
Jonathan R. Warden <john.warden@gmail.com>
2022-12-05 21:41:46 +0100

    speed up penalties calculation

--- sql/previous-crawl-view.sql
+++ sql/previous-crawl-view.sql
@@ -2,7 +2,7 @@
 -- It is a bit tricky because the sampleTime may be different for each story, because
 -- Some stories may appear and disappear from crawl results if they fall off the front page and reappear.
 
-create view previousCrawl as
+create temporary table previousCrawl as
 with latest as (
   select * from dataset
   where sampleTime = (select max(sampleTime) from dataset)
