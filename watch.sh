42285ecffddc3992f3bff6c918ca1e8a2730ce2a
Jonathan R. Warden <john.warden@gmail.com>
2022-12-30 16:35:15 +0100

    Conditionally add humanlog to watch.sh
    There is not a nix package for humanlog yet, so make it optional

--- watch.sh
+++ watch.sh
@@ -1,1 +1,5 @@
-ls *.go **/**.tmpl **/**.sql | entr -ncr sh -c 'go install; go run *.go' 
+if which humanlog ; then
+	LOGFORMATTER="| humanlog --truncate=0"
+fi
+
+ls *.go **/**.tmpl **/**.sql | entr -ncr sh -c "go install; go run *.go $LOGFORMATTER"
