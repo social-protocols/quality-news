da18382fdde2855c8bd35d54a3c898de3ba885fb
Jonathan R. Warden <john.warden@gmail.com>
2023-03-03 21:10:29 +0100

    Create test-redirect URL to test 301 redirect logic (#74)

--- httpserver.go
+++ httpserver.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	// "github.com/dyninc/qstring"
@@ -57,6 +59,8 @@ func (app app) httpServer(onPanic func(error)) *http.Server {
 	router.GET("/stats", middleware("stats", l, onPanic, app.statsHandler()))
 	router.GET("/about", middleware("about", l, onPanic, app.aboutHandler()))
 
+	router.GET("/test-redirect", middleware("test-redirect", l, onPanic, app.testRedirectHandler()))
+
 	server.Handler = app.cacheAndCompressMiddleware(router)
 
 	return server
@@ -107,5 +111,40 @@ func (app app) serveFiles(root http.FileSystem) func(w http.ResponseWriter, r *h
 		w.Header().Set("Cache-Control", "public, max-age=86400") // 1 hours
 		r.URL.Path = p.ByName("filepath")
 		fileServer.ServeHTTP(w, r)
+	}
+}
+
+var nonCanonicalDomains = map[string]string{
+	"social-protocols-news.fly.dev:443": "news.social-protocols.org",
+	"127.0.0.1:8080":                    "localhost:8080", // just for testing
+}
+
+var canonicalDomains = getValues(nonCanonicalDomains)
+
+func (app app) testRedirectHandler() func(http.ResponseWriter, *http.Request, struct{}) error {
+	logger := app.logger
+	return func(w http.ResponseWriter, r *http.Request, _ struct{}) error {
+		// Redirect any non-canonical domain to the corresponding canonical domain.
+		for nonCanonicalDomain, canonicalDomain := range nonCanonicalDomains {
+			if r.Host == nonCanonicalDomain {
+				url := "https://" + canonicalDomain + r.RequestURI
+				logger.Info("Redirecting to", "url", url, "request_host", r.Host)
+				http.Redirect(w, r, url, 301)
+				return nil
+			}
+		}
+		isCanonical := false
+		for _, canonicalDomain := range canonicalDomains {
+			if strings.HasPrefix(r.Host, canonicalDomain) {
+				isCanonical = true
+				break
+			}
+		}
+		if !isCanonical {
+			return fmt.Errorf("Invalid request host: %s", r.Host)
+		}
+
+		_, _ = w.Write([]byte("Hello, World!\n"))
+		return nil
 	}
 }
--- utils.go
+++ utils.go
@@ -9,3 +9,13 @@ func getKeys[K comparable, V comparable](m map[K]V) []K {
 	}
 	return keys
 }
+
+func getValues[K comparable, V any](m map[K]V) []V {
+	values := make([]V, len(m))
+	var i int
+	for _, value := range m {
+		values[i] = value
+		i++
+	}
+	return values
+}
