c85ea7b444004ec1b44aa8f3766832f3a8998ea0
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-03-17 09:39:24 +0100

    calculate upvoterate on the fly

--- frontpage.go
+++ frontpage.go
@@ -255,8 +255,7 @@ func getFrontPageStories(ctx context.Context, ndb newsDatabase, ranking string, 
 			case "highdelta":
 				orderBy = "case when rawRank is null or (topRank is null and rawRank > 90) then null else ifnull(topRank,91) - rawRank end nulls last"
 			case "best-upvoterate":
-				println("best-upvoterate")
-				orderBy = "upvoteRate desc nulls last"
+				orderBy = fmt.Sprintf("(cumulativeUpvotes + priorWeight)/((1-exp(-fatigueFactor*cumulativeExpectedUpvotes))/fatigueFactor + priorWeight) desc nulls last")
 			default:
 				orderBy = fmt.Sprintf("%sRank nulls last", ranking)
 			}
