b9700446fef584a684467132e28e8689a5787a9c
Jonathan R. Warden <john.warden@gmail.com>
2023-04-25 14:18:32 +0200

    Revert "Delete data more than 30 days old so we don't run out of disk space (fixes #54)"
    This reverts commit 22913c5939c22fcbc8406ed6efa1c0a153d6d15f.

--- database.go
+++ database.go
@@ -92,11 +92,6 @@ func (ndb newsDatabase) init() error {
 		`
 		drop view if exists previousCrawl
 		`,
-
-		// delete data older than one month. It's taking about 18 days to fill a gig, and we have
-		// 3 gig volume currently. That gives us 54 days. But 30 days is plenty and this gives us margin
-		// in case the rate of growth of data increases.
-		`delete from dataset where sampletime <= unixepoch()-30*24*60*60`,
 	}
 
 	for _, s := range seedStatements {
