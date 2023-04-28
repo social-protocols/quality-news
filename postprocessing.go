c96b355fe2fdc9b455e27534e4968c9f86c8394f
Jonathan R. Warden <john.warden@gmail.com>
2023-04-28 17:17:11 -0600

    Delete old data at the end of each crawl (further fixes #54)

--- postprocessing.go
+++ postprocessing.go
@@ -24,7 +24,12 @@ func (app app) crawlPostprocess(ctx context.Context, tx *sql.Tx) error {
 	var err error
 
 	// for _, filename := range []string{"previous-crawl.sql", "resubmissions.sql", "raw-ranks.sql", "upvote-rates.sql"} {
-	for _, filename := range []string{"previous-crawl.sql", "resubmissions.sql", "raw-ranks.sql"} {
+	for _, filename := range []string{
+		"previous-crawl.sql",
+		"resubmissions.sql",
+		"raw-ranks.sql",
+		"delete-old-data.sql",
+	} {
 		err = app.ndb.executeSQLFile(ctx, tx, filename)
 		if err != nil {
 			return err
