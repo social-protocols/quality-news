bbad4909de06e0c95014b14b2a59e49d50c4e2fb
Jonathan R. Warden <john.warden@gmail.com>
2023-08-30 12:39:56 +0200

    Increase data retention window from 30 to 45 days in delete-old-data.sql
    This is so that some of my votes from a few weeks ago aren't deleted while I figure out a better way to retain data for stories that have been upvoted

--- sql/delete-old-data.sql
+++ sql/delete-old-data.sql
@@ -1,4 +1,4 @@
 -- delete data older than one month. It's taking about 18 days to fill a gig, and we have
 -- 3 gig volume currently. That gives us 54 days. But 30 days is plenty and this gives us margin
 -- in case the rate of growth of data increases.
-delete from dataset where sampletime <= unixepoch()-30*24*60*60
+delete from dataset where sampletime <= unixepoch()-45*24*60*60
