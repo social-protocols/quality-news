7d36b26591a68edb4f82ad7e59b06305dd7c0779
Jonathan R. Warden <john.warden@gmail.com>
2024-11-18 11:57:43 -0700

    Add missing algorithms pages

---
+++ algorithmspage.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+type AlgorithmsPageData struct {
+	DefaultPageHeaderData
+}
+
+func (d AlgorithmsPageData) IsAlgorithmsPage() bool {
+	return true
+}
+
+func (app app) algorithmsHandler() func(http.ResponseWriter, *http.Request, struct{}) error {
+	return func(w http.ResponseWriter, r *http.Request, p struct{}) error {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		err := templates.ExecuteTemplate(w, "about.html.tmpl", AlgorithmsPageData{DefaultPageHeaderData{UserID: app.getUserID(r)}})
+
+		return errors.Wrap(err, "executing Algorithms page template")
+	}
+}
