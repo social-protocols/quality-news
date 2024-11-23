5d6e9fb615fa82dd6dff83a1b0349dff757738a4
Jonathan R. Warden <john.warden@gmail.com>
2024-11-22 17:55:13 -0700

    Revert last change to delete-old-data.sql because it is timing out

--- sql/delete-old-data.sql
+++ sql/delete-old-data.sql
@@ -1,5 +1,4 @@
 -- delete data older than one month. It's taking about 18 days to fill a gig, and we have
 -- 3 gig volume currently. That gives us 54 days. But 30 days is plenty and this gives us margin
 -- in case the rate of growth of data increases.
-delete from dataset where id in (select distinct id from dataset where sampletime <= unixepoch()-28*24*60*60);
-update stories set archived = 0;
+delete from dataset where sampletime <= unixepoch()-30*24*60*60
