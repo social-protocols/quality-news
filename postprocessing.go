8a1d3dce69ec35a57a230578c0d65146f100459e
Jonathan R. Warden <john.warden@gmail.com>
2025-02-04 10:49:37 +0100

    Remove penalties logic

--- postprocessing.go
+++ postprocessing.go
@@ -45,7 +45,6 @@ func (app app) crawlPostprocess(ctx context.Context, tx *sql.Tx) error {
 		"previous-crawl.sql",
 		"resubmissions.sql",
 		"raw-ranks.sql",
-		"penalties.sql",
 	} {
 		err = executeSQLFile(ctx, tx, filename)
 		if err != nil {
