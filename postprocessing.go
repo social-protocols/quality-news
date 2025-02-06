f21a97daa5cf4da43ae595233436d3efe938f432
Jonathan R. Warden <john.warden@gmail.com>
2025-02-06 11:33:12 +0100

    Reactivate delete-old-data.sql

--- postprocessing.go
+++ postprocessing.go
@@ -45,6 +45,7 @@ func (app app) crawlPostprocess(ctx context.Context, tx *sql.Tx) error {
 		"previous-crawl.sql",
 		"resubmissions.sql",
 		"raw-ranks.sql",
+		"delete-old-data.sql",
 	} {
 		err = executeSQLFile(ctx, tx, filename)
 		if err != nil {
