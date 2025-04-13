033da5063f40df427e6a0d2ecaf77f44d7334c12
Jonathan R. Warden <john.warden@gmail.com>
2025-04-13 13:50:57 +0200

    Move archiving limit back down to 2

--- database.go
+++ database.go
@@ -358,7 +358,7 @@ func (ndb newsDatabase) selectStoriesToArchive(ctx context.Context) ([]int, erro
 			where sampleTime <= strftime('%s', 'now') - 21*24*60*60
 			and score > 2
 		)
-		select distinct(id) from latest limit 10
+		select distinct(id) from latest limit 2
 	`
 
 	// Check context before query
